d38b33b92d32b3f498059f2ede77bd6a60a904f9
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2023-12-31 02:11:38 +0600

    NEW: AUTO candidate or Hr account create on signup

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from base.model import BaseModel
 import uuid
+from hr.models import Company
 
 
 class User(AbstractUser):
@@ -37,27 +38,6 @@ class CandidateProfile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='candidate_profile')
     passing_date = models.DateField(blank=True, null=True)
     resume = models.FileField(upload_to='resumes/%Y', blank=True)
-    full_name = models.CharField(max_length=200, blank=True)
-    address = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=200, blank=True)
-    state = models.CharField(max_length=200, blank=True)
-    country = models.CharField(max_length=200, blank=True)
-    postal_code = models.CharField(max_length=200, blank=True)
-
-    def __str__(self):
-        return self.user
-
-    def save(self):
-        self.full_name = self.user.get_full_name()
-        super(HrProfile, self).save()
-
-
-class HrProfile(BaseModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hr_profile')
-    company_name = models.CharField(max_length=200, blank=True)
-    biography = models.TextField(blank=True, max_length=1000)
-    full_name = models.CharField(max_length=200, blank=True)
-    position = models.CharField(max_length=200, blank=True)
     address = models.CharField(max_length=200, blank=True)
     city = models.CharField(max_length=200, blank=True)
     state = models.CharField(max_length=200, blank=True)
@@ -67,13 +47,32 @@ class HrProfile(BaseModel):
     def __str__(self):
         return self.user.email
 
-    def save(self):
-        self.full_name = self.user.get_full_name()
-        super(HrProfile, self).save()
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         candidate = CandidateProfile.objects.create(user=instance)
-#         candidate.save()
+class HrProfile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hr_profile')
+    biography = models.TextField(blank=True, max_length=1000)
+    position = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    postal_code = models.CharField(max_length=200, blank=True)
+    company = models.ForeignKey(Company, on_delete = models.CASCADE, blank= True, default='46821c24-d5d6-4d40-b426-3aebcfcedc85' )
+
+    def __str__(self):
+        return self.user.email
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        if created:
+            if instance.is_hr:
+                HrProfile.objects.create(user=instance)
+            else:
+                candidate = CandidateProfile.objects.create(user=instance)
+                candidate.save()
+    except Exception as e:
+        print(e)
