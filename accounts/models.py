5445641109a41cdae2b049d4b68a818176e0d831
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2024-01-01 22:15:52 +0600

    Refactor: here and there

--- accounts/models.py
+++ accounts/models.py
@@ -58,7 +58,7 @@ class HrProfile(BaseModel):
     state = models.CharField(max_length=200, blank=True)
     country = models.CharField(max_length=200, blank=True)
     postal_code = models.CharField(max_length=200, blank=True)
-    company = models.ForeignKey(Company, on_delete = models.CASCADE, blank= True )
+    company = models.ForeignKey(Company, on_delete = models.CASCADE, blank= True)
 
     def __str__(self):
         return self.user.email
